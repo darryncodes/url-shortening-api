c7df463f1a83818d6d5b3f18474a6246be52b3a1
Darryn Codes <darryncodes@gmail.com>
2023-03-07 15:18:52 +0000

    feat: adding local storage logic

--- src/components/Output/Output.js
+++ src/components/Output/Output.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Button from "react-bootstrap/Button";
 
 import styles from "./Output.module.scss";
@@ -6,8 +6,9 @@ import styles from "./Output.module.scss";
 function Output(props) {
     const shortLink = useRef("");
     const [copySuccess, setCopySuccess] = useState("Copy");
+    const [localLinks, setLocalLinks] = useState([]);
 
-    const copyToClipbpard = () => {
+    const copyToClipboard = () => {
         navigator.clipboard.writeText(shortLink.current.innerText);
         setCopySuccess("Copied!");
         setTimeout(() => {
@@ -15,9 +16,30 @@ function Output(props) {
         }, 1750);
     };
 
+    useEffect(() => {
+        const saveToLocalStorage = () => {
+            localStorage.setItem("links", JSON.stringify(props.output));
+        };
+        if (props.output.length !== 0) {
+            saveToLocalStorage();
+        }
+    }, [props.output]);
+
+    useEffect(() => {
+        const checkLocalStorage = () => {
+            const links = JSON.parse(localStorage.getItem("links"));
+            if (links) {
+                setLocalLinks(links);
+            }
+        };
+        checkLocalStorage();
+    }, []);
+
+    const finalLinks = [...localLinks, ...props.output];
+
     return (
         <>
-            {props.output.map((links) => (
+            {finalLinks.map((links) => (
                 <div className={styles.output} key={links.id}>
                     <p className={styles.output__link}>{links.link}</p>
                     <div className={styles.output__result}>
@@ -30,7 +52,7 @@ function Output(props) {
                         <Button
                             variant="success"
                             className={styles.output__btn}
-                            onClick={copyToClipbpard}
+                            onClick={copyToClipboard}
                         >
                             {copySuccess}
                         </Button>
