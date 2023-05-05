fcaeb79eecbb67786507e59a5d8d9dbd95a53dd8
Darryn Codes <darryncodes@gmail.com>
2023-05-05 18:28:25 +0100

    fix - moving mmain landmark tag

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,18 +1,20 @@
-import React from "react";
+import React from 'react';
 
-import Navigation from "../Navigation/Navigation";
-import Hero from "../Hero/Hero";
-import Body from "../Body/Body";
-import Links from "../Links/Links";
-import Footer from "../Footer/Footer";
+import Navigation from '../Navigation/Navigation';
+import Hero from '../Hero/Hero';
+import Body from '../Body/Body';
+import Links from '../Links/Links';
+import Footer from '../Footer/Footer';
 
 function Home() {
     return (
         <>
             <Navigation />
             <Hero />
-            <Body />
-            <Links />
+            <main>
+                <Body />
+                <Links />
+            </main>
             <Footer />
         </>
     );
